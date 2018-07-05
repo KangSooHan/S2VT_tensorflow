aedc1e7bcaa4f4ff4e79b75a242dbf85c79bb382
KangSooHan <tngkssapf@gmail.com>
2018-07-06 06:18:14 +0900

    extract_RGB_feature to extract_RGB_feature_ten

--- extract_RGB_feature.py
+++ extract_RGB_feature.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import skimage
-from cnn_util import *
+from cnn_util_tensorflow import *
 import pdb
 
 def preprocess_frame(image, target_height=224, target_width=224):
@@ -25,50 +25,68 @@ def preprocess_frame(image, target_height=224, target_width=224):
         resized_image = cv2.resize(image, (target_height, int(height*float(target_width)/width)))
         cropping_length = int((resized_image.shape[0]-target_width) / 2)
         resized_image = resized_image[cropping_length:resized_image.shape[0]-cropping_length,:]
-
     return cv2.resize(resized_image, (target_height, target_width))
+
 
 def main():
     num_frames = 80
-    vgg_model = './vgg/VGG_ILSVRC_16_layers.caffemodel'
-    vgg_deploy = './vgg/VGG_ILSVRC_16_layers_deploy.prototxt'
     video_path = './data/youtube_videos'
     video_save_path = './rgb_feats'
     videos = os.listdir(video_path)
-    videos = filter(lambda x: x.endswith('avi'), videos)
+    videos = filter(lambda x: x.endswith('.avi'), videos)
 
-    cnn = CNN(model=vgg_model, deploy=vgg_deploy, width=224, height=224)
-    pdb.set_trace()
-    for idx, video in enumerate(videos):
-        print(idx, video)
-        if os.path.exists(os.path.join(video_save_path, video)):
-            print('Already processed...')
-            continue
-        video_fullpath = os.path.join(video_path, video)
-        try:
-            cap = cv2.VideoCapture(video_fullpath)
-        except:
-            pass
-        frame_count = 0
-        frame_list = []
-        while True:
-            ret, frame = cap.read()
-            if ret is False:
-                break
-            frame_list.append(frame)
-            frame_count += 1
-        frame_list = np.array(frame_list)
 
-#        pdb.set_trace()
-        if frame_count > 80:
-            frame_indices = np.linspace(0, frame_count, num=num_frames, endpoint=False).astype(int)
-            frame_list = frame_list[frame_indices]
-        
-        cropped_frame_list = np.array(list(map(lambda x: preprocess_frame(x), frame_list)))
-        feats = cnn.get_features(cropped_frame_list)
-        
-        save_full_path = os.path.join(video_save_path, video + '.npy')
-        np.save(save_full_path, feats)
+    input_data = tf.placeholder(tf.float32, shape=(num_frames, 224, 224, 3), name="X")
+    feats = CNN(input_data)
+    value = feats.fc7()
+
+    init = tf.global_variables_initializer()
+
+    #pdb.set_trace()
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+        with tf.variable_scope("vgg16") as scope:
+            for idx, video in enumerate(videos):
+                print(idx, video)
+                if os.path.exists(os.path.join(video_save_path, video)):
+                    print('Already processed...')
+                    continue
+                video_fullpath = os.path.join(video_path, video)
+                try:
+                    cap = cv2.VideoCapture(video_fullpath)
+                except:
+                    pass
+                frame_count = 0
+                frame_list = []
+                while True:
+                    ret, frame = cap.read()
+                    if ret is False:
+                        break
+                    frame_list.append(frame)
+                    frame_count += 1
+                frame_list = np.array(frame_list)
+                #pad_frame_list = np.zeros(frame_list.shape)
+
+                #pdb.set_trace()
+                #print(frame_count)
+                if frame_count >= num_frames:
+                    frame_indices = np.linspace(0, frame_count, num=num_frames, endpoint=False).astype(int)
+                    frame_list = frame_list[frame_indices]
+                elif frame_count != 0:
+                    frame_list = np.pad(frame_list, ((0,num_frames-frame_count),(0,0),(0,0),(0,0)), 'constant')
+                cropped_frame_list = np.array(list(map(lambda x: preprocess_frame(x), frame_list)))
+                #print(cropped_frame_list.shape)
+
+                if idx==0:
+                    sess.run(init)
+                    real_sess = feats.load_weights('vgg16_weights.npz', sess)
+
+                if cropped_frame_list.shape[0] == 0 :
+                    pass
+                else:
+                    Save = sess.run(value, feed_dict={input_data:cropped_frame_list[:]})
+
+                    save_full_path = os.path.join(video_save_path, video + '.npy')
+                    np.save(save_full_path, Save)
 
 if __name__=="__main__":
     main()
